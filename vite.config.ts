0619a64f7713c629b62eb13a61f2bee91e1d5932
EE Azura <EE_Azura@outlook.com>
2025-03-19 21:49:56 +0800

    build: initial

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+const packageName = 'breeze-request';
+
+export default defineConfig({
+  build: {
+    lib: {
+      // 使用多入口配置
+      entry: {
+        [packageName]: resolve(__dirname, 'src/index.ts'),
+        types: resolve(__dirname, 'src/types-export.ts')
+      },
+      formats: ['es', 'cjs'],
+      fileName: (format, entryName) => {
+        if (entryName === packageName) {
+          return format === 'es' ? `${packageName}.js` : `${packageName}.cjs`;
+        }
+        return format === 'es' ? `${entryName}.js` : `${entryName}.cjs`;
+      }
+    },
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        preserveModules: false
+      }
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'node'
+  },
+  plugins: [dts()]
+});
